bd158c340172b09587b71c52db149881ad4504bd
jinxin <jinxin@dragonest.com>
2020-11-17 23:49:26 +0800

    ball, paddle, ground generated, gate and bounce effect left.

--- testbed/tests/hello_world.cpp
+++ testbed/tests/hello_world.cpp
@@ -1,6 +1,5 @@
 #include "test.h"
 #include <memory>
-#include <iostream>
 
 
 class hello_world final: public Test {
@@ -19,16 +18,32 @@ public:
 			ground_body_->CreateFixture(&ground_box, 0.0f);
 		}		
 
-		// dynamic body
+		// paddle
 		{
 			b2BodyDef bd;
 			bd.type = b2_dynamicBody;
-			bd.position.Set(0.0f, 10.0f);
-			bd.angle = 10.0f;
-			ball_ = m_world->CreateBody(&bd);
+			bd.position.Set(0.0f, 0.0f);			
+			paddle_ = m_world->CreateBody(&bd);
 
 			b2PolygonShape ball_box;
 			ball_box.SetAsBox(4.0f, 1.0f);
+
+			/*b2CircleShape circle;
+			circle.m_radius = 1.5f;*/
+
+			b2FixtureDef fd;
+			fd.shape = &ball_box;
+			fd.density = 1.0f;
+			fd.friction = 0.4f;
+			paddle_->CreateFixture(&fd);
+		}
+
+		// ball
+		{
+			b2BodyDef bd;
+			bd.type = b2_dynamicBody;
+			bd.position.Set(0.0f, 20.0f);			
+			ball_ = m_world->CreateBody(&bd);			
 
 			b2CircleShape circle;
 			circle.m_radius = 1.5f;
@@ -38,7 +53,13 @@ public:
 			fd.density = 1.0f;
 			fd.friction = 0.4f;
 			ball_->CreateFixture(&fd);
-		}							
+		}
+	}
+
+	void stop_paddle() const	
+	{		
+		paddle_->SetLinearVelocity(b2Vec2(0.0f, 0.0f));
+		paddle_->SetAngularVelocity(0.0f);
 	}
 
 	void Keyboard(const int key) override
@@ -56,9 +77,15 @@ public:
 		}
 	}
 
+	void KeyboardUp(int key) override
+	{
+		mv_state_ = move_state::ems_idle;
+		stop_paddle();
+	}
+
 	void Step(Settings& settings) override
 	{		
-		b2Vec2 pos = ball_->GetPosition();		
+		b2Vec2 pos = paddle_->GetPosition();		
 		switch (mv_state_)
 		{
 		case move_state::ems_left:
@@ -67,10 +94,11 @@ public:
 		case move_state::ems_right:
 			pos.x += 0.5f;
 			break;
-		default:			
+		default:
+			stop_paddle();
 			break;
 		}
-		ball_->SetTransform(pos, ball_->GetAngle());
+		paddle_->SetTransform(pos, paddle_->GetAngle());		
 		
 		Test::Step(settings);
 	}
